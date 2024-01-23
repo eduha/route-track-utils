1517f6dfb0cfb5e57de32d2667ccacd16f833555
Eduard Faber <admin@eduha.info>
2024-01-24 00:30:08 +0600

    команды, убрал консоль

--- src/methods/randonneurs.kz/file-convert-bot.js
+++ src/methods/randonneurs.kz/file-convert-bot.js
@@ -19,82 +19,82 @@ module.exports = async (req, res) => {
         const client = new SecretManagerServiceClient();
         const [secret] = await client.accessSecretVersion({name: `projects/track-utils/secrets/RANDONNEURS_BOT_TOKEN/versions/latest`});
         token = Buffer.from(secret.payload.data).toString();
-        console.log({token}, 'secret');
       }
       catch (e) {
         console.error(e);
         token = req.query?.token;
-        console.log({token}, 'query');
       }
 
-      const allowed = ['kml', 'kmz', 'gpx'];
-      const match = message?.document?.file_name?.match?.(new RegExp(`\.(${allowed.join('|')})$`, 'i'));
+      if (token && message?.document?.file_name) {
+        const allowed = ['kml', 'kmz', 'gpx'];
+        const match = message.document.file_name?.match?.(new RegExp(`\.(${allowed.join('|')})$`, 'i'));
 
-      if (token && message?.document && match) {
-        const {document} = message;
+        if (message?.document && match) {
+          const {document} = message;
 
-        const {result} = await got(`https://api.telegram.org/bot${token}/getFile`, {
-          searchParams: {
-            file_id: document?.file_id,
-          },
-        }).json();
+          const {result} = await got(`https://api.telegram.org/bot${token}/getFile`, {
+            searchParams: {
+              file_id: document?.file_id,
+            },
+          }).json();
 
-        const from = match[1].toLowerCase();
+          const from = match[1].toLowerCase();
 
-        const source = `https://api.telegram.org/file/bot${token}/${result.file_path}`;
-        const geoData = await geoJSON((await got(source, {responseType: 'buffer'})).body, from);
+          const source = `https://api.telegram.org/file/bot${token}/${result.file_path}`;
+          const geoData = await geoJSON((await got(source, {responseType: 'buffer'})).body, from);
 
-        let track, filename, snippet;
-        const checkpoints = [];
+          let track, filename, snippet;
+          const checkpoints = [];
 
-        geoData?.features?.forEach?.(({properties, geometry}) => {
-          if (geometry?.type === 'LineString') {
-            track = geometry?.coordinates || [];
-            filename = properties?.name || 'track';
+          geoData?.features?.forEach?.(({properties, geometry}) => {
+            if (geometry?.type === 'LineString') {
+              track = geometry?.coordinates || [];
+              filename = properties?.name || 'track';
+            }
+            else if (geometry?.type === 'Point') {
+              checkpoints.push({
+                coordinates: geometry?.coordinates || [],
+                name: properties?.name || 'Point',
+              });
+            }
+          });
+
+          do {
+            snippet = JSON.stringify({
+              n: filename,
+              c: checkpoints,
+              t: polyline.encode(track, 5).toString(),
+            });
+
+            if (snippet.length < 50000) {
+              break;
+            }
+
+            track = turf.getCoords(turf.simplify(turf.lineString(track), {
+              highQuality: true,
+              tolerance: 0.00001,
+            }));
           }
-          else if (geometry?.type === 'Point') {
-            checkpoints.push({
-              coordinates: geometry?.coordinates || [],
-              name: properties?.name || 'Point',
+          while (true);
+
+          const key = await writeAs(snippet);
+
+          if (key) {
+            return res.send({
+              method: 'sendMessage',
+              chat_id: message?.chat?.id,
+              text: `https://route.eduha.info/?snippet=${encodeURIComponent(key)}`,
+              parse_mode: 'HTML',
+              link_preview_options: {
+                is_disabled: true,
+              },
+              disable_notification: true,
+              reply_parameters: {
+                message_id: message?.message_id,
+                chat_id: message?.chat?.id,
+              },
             });
           }
-        });
-
-        do {
-          snippet = JSON.stringify({
-            n: filename,
-            c: checkpoints,
-            t: polyline.encode(track, 5).toString(),
-          });
-
-          if (snippet.length < 50000) {
-            break;
-          }
-
-          track = turf.getCoords(turf.simplify(turf.lineString(track), {
-            highQuality: true,
-            tolerance: 0.00001,
-          }));
-        }
-        while (true);
-
-        const key = await writeAs(snippet);
-
-        if (key) {
-          return res.send({
-            method: 'sendMessage',
-            chat_id: message?.chat?.id,
-            text: `https://route.eduha.info/?snippet=${encodeURIComponent(key)}`,
-            parse_mode: 'HTML',
-            link_preview_options: {
-              is_disabled: true,
-            },
-            disable_notification: true,
-            reply_parameters: {
-              message_id: message?.message_id,
-              chat_id: message?.chat?.id,
-            },
-          });
         }
       }
     }
