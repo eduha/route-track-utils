2a5ddb3f89daa4a8632952fc77060f82a4d0d65c
Eduard Faber <admin@eduha.info>
2024-01-24 03:27:07 +0600

    parallel

--- src/helpers/writeas.js
+++ src/helpers/writeas.js
@@ -1,20 +1,24 @@
 const got = require('got');
+const promiseLimit = require('promise-limit');
+const limit = promiseLimit(1);
 
 module.exports = async data => {
-  if (typeof data !== 'string') {
-    data = JSON.stringify(data);
-  }
+  return await limit(async () => {
+    if (typeof data !== 'string') {
+      data = JSON.stringify(data);
+    }
 
-  const {body} = await got.post('https://write.as/api/posts', {
-    json: {
-      body: data,
-    },
-    responseType: 'json',
+    const {body} = await got.post('https://write.as/api/posts', {
+      json: {
+        body: data,
+      },
+      responseType: 'json',
+    });
+
+    if (body.code === 201 && body?.data?.id) {
+      return body.data.id;
+    }
+
+    throw 'no write.as id';
   });
-
-  if (body.code === 201 && body?.data?.id) {
-    return body.data.id;
-  }
-
-  throw 'no write.as id';
 };
